f77d44d338f0bab6089d731109dd7816eb79eb68
pavel rodionov <pavel.rodionov.developer@gmail.com>
2023-07-24 15:05:40 +0800

    info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,20 @@ import React from "react"
 class Info extends React.Component {
     render() {
         return (
-            <header>{this.props.title}</header>
+            <div class="baseColor infoBlock">
+                <h2>Информация о маршрутах</h2>
+                <br/>
+                <p>Направления движения трамваев нашего города вы можете узнать на интерактивной карте 2ГИС</p>
+                <div class="tram_number">
+                    <h3>Трамваи:</h3>
+                    <p><a class="btn" href="https://2gis.ru/ulanude/route/5207892659535917" target="_blank">№ 1</a></p>
+                    <p><a class="btn" href="https://2gis.ru/ulanude/route/5207892659535916" target="_blank">№ 2</a></p>
+                    <p><a class="btn" href="https://2gis.ru/ulanude/route/5207892659535918" target="_blank">№ 4</a></p>
+                    <p><a class="btn" href="https://2gis.ru/ulanude/route/5207892659535919" target="_blank">№ 5</a></p>
+                    <p><a class="btn" href="https://2gis.ru/ulanude/route/5207892914730739" target="_blank">№ 7</a></p>
+                    <p><a class="btn" href="https://2gis.ru/ulanude/route/5207892914736053" target="_blank">№ 8</a></p>
+                </div>
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/main.css'
 import './css/banner.css'
+import './css/info.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
