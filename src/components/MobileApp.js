294c379c75d1f5652431defb26651caba15efc11
pavel rodionov <pavel.rodionov.developer@gmail.com>
2023-07-29 22:56:37 +0800

    bus03 proezd03 components

--- src/components/MobileApp.js
+++ src/components/MobileApp.js
@@ -3,7 +3,13 @@ import React from "react"
 class MobileApp extends React.Component {
     render() {
         return (
-            <header>{this.props.title}</header>
+            <div className="baseColor block">
+                <p>Следить за движением трамваев в режиме онлайн можно на странице проекта BUS03</p>
+                <br/>
+                <p>а также с помощью приложения "Умный Транспорт"</p>
+                <br/>
+                <a className="btn btn-default" href="http://bus03.ru/" target="_blank">Перейти</a>
+            </div>
         )
     }
 }
--- src/components/Proezd03.js
+++ src/components/Proezd03.js
@@ -3,7 +3,13 @@ import React from "react"
 class Proezd03 extends React.Component {
     render() {
         return (
-            <header>{this.props.title}</header>
+            <div className="baseColorGreen block">
+                <h1>Хотите купить электронный проездной?</h1>
+                <br/>
+                <p>Оформить недельный проездной на 15 поездок вы можете по ссылке:</p>
+                <br/>
+                <a className="btn btn-default" href="https://proezd03.ru/site/travelcard-development" target="_blank">Купить электронный проездной</a>
+            </div>
         )
     }
 }
