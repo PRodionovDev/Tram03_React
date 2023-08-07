3a6e930aa4dcb6250d6390cd814d6ddaabd35279
pavel rodionov <pavel.rodionov.developer@gmail.com>
2023-08-07 13:26:14 +0800

    navbar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,19 @@ import React from "react"
 class Header extends React.Component {
     render() {
         return (
-            <header>{this.props.title}</header>
+            <nav>
+                <div className="logo"></div>
+                <div>
+                <ul>
+                    <li><a href="#anchor-index">Главная</a></li>
+                    <li><a href="#anchor-info">Пассажирам</a></li>
+                    <li><a href="#anchor-scheme">Схема</a></li>
+                    <li><a href="#anchor-bus03">Умный транспорт</a></li>
+                    <li><a href="#anchor-proezd03">Проездной</a></li>
+                    <li><a href="#anchor-faq">Информация</a></li>
+                </ul>
+                </div>
+          </nav>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './css/banner.css'
 import './css/info.css'
 import './css/scheme.css'
 import './css/footer.css'
+import './css/navbar.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
